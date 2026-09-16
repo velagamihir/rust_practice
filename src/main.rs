2f61d8ed78f5b341438bc338d11267c27f979db0
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-16 21:06:51 +0530

    feat: add basic system calculations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 fn main() {
-    let os_name: &str = "MihirOS";
-    let os_version: &str = "0.0.1";
-    let ram: u8 = 8;
+    let total_ram: u32 = 16;
+    let used_ram: u32 = 6;
+
+    let free_ram = total_ram - used_ram;
+
+    println!("Total RAM: {} GB", total_ram);
+    println!("Used RAM: {} GB", used_ram);
+    println!("Free RAM: {} GB", free_ram);
+
     let cpu_cores: u8 = 8;
-    let cpu_speed: &str = "16ghz";
-    let kernel_running: bool = true;
-    println!("==============");
-    println!("MihirOS Hardware");
-    println!("==============");
-    println!(
-        "OS: {}\nVersion: {}\nRAM: {}\nCPU Cores: {}\nCPU Speed: {}\nKernel Running: {}",
-        os_name, os_version, ram, cpu_cores, cpu_speed, kernel_running
-    );
+    let threads_per_core: u8 = 2;
+    let total_threads = cpu_cores * threads_per_core;
+
+    //Memory blocks
+    let total_memory = 16;
+    let each_block_size = 2;
+    let number_of_blocks = total_memory / each_block_size;
+
+    //Printing values
+    println!("Total number of threads: {}", total_threads);
+    println!("Number of blocks: {}", number_of_blocks);
 }
