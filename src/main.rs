4fb7d93719cd1d0a8091a994dcb0d2e60935a3f5
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-16 20:15:28 +0530

    feat: add MihirOS system metadata

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    println!("===============");
-    println!("MIHIR OS");
-    println!("===============");
-    println!("Booting....");
-    println!("Loading kernel....");
-    println!("Initializing hardware....");
-    println!("\nWelcome to MihirOS!");
+    let os_name = "MihirOS";
+    let mut os_version = "0.0.1";
+    let developer = "Mihir Velaga";
+    println!(
+        "The OS Name is: {}\nThe Version is: {}\nThe Developer is: {}",
+        os_name, os_version, developer
+    );
+    os_version = "0.0.2";
+    println!("os_version: {}", os_version);
 }
