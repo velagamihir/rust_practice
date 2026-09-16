0905440955dcb05294cd0fa3ab0361e758eb28e9
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-16 20:45:06 +0530

    feat: add MihirOS hardware information

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
-    let os_name = "MihirOS";
-    let mut os_version = "0.0.1";
-    let developer = "Mihir Velaga";
+    let os_name: &str = "MihirOS";
+    let os_version: &str = "0.0.1";
+    let ram: u8 = 8;
+    let cpu_cores: u8 = 8;
+    let cpu_speed: &str = "16ghz";
+    let kernel_running: bool = true;
+    println!("==============");
+    println!("MihirOS Hardware");
+    println!("==============");
     println!(
-        "The OS Name is: {}\nThe Version is: {}\nThe Developer is: {}",
-        os_name, os_version, developer
+        "OS: {}\nVersion: {}\nRAM: {}\nCPU Cores: {}\nCPU Speed: {}\nKernel Running: {}",
+        os_name, os_version, ram, cpu_cores, cpu_speed, kernel_running
     );
-    os_version = "0.0.2";
-    println!("os_version: {}", os_version);
 }
