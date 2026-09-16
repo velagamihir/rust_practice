2f54606422dbd5d314c7c2af44023a29a2e2990e
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-16 21:16:27 +0530

    feat: add system requirements check

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 fn main() {
-    let total_ram: u32 = 16;
-    let used_ram: u32 = 6;
-
-    let free_ram = total_ram - used_ram;
-
-    println!("Total RAM: {} GB", total_ram);
-    println!("Used RAM: {} GB", used_ram);
-    println!("Free RAM: {} GB", free_ram);
-
-    let cpu_cores: u8 = 8;
-    let threads_per_core: u8 = 2;
-    let total_threads = cpu_cores * threads_per_core;
-
-    //Memory blocks
-    let total_memory = 16;
-    let each_block_size = 2;
-    let number_of_blocks = total_memory / each_block_size;
-
-    //Printing values
-    println!("Total number of threads: {}", total_threads);
-    println!("Number of blocks: {}", number_of_blocks);
+    let ram = 16;
+    let cpu_cores = 8;
+    let cpu_threads = 16;
+    let kernel_running = false;
+    println!("=========");
+    println!("MihirOS Check!");
+    println!("=========");
+    println!(
+        "RAM: {} GB\nCPU Cores: {}\nCPU Threads: {}",
+        ram, cpu_cores, cpu_threads
+    );
+    let ram_pass = ram >= 8;
+    let cpu_cores_pass = (cpu_cores >= 4) && (cpu_threads >= 8);
+    if kernel_running {
+        println!("RUNNING...");
+    } else {
+        println!("Starting Kernel");
+    }
+    let mut system_req = "FAIL";
+    if ram_pass && cpu_cores_pass {
+        system_req = "PASS";
+    }
+    println!("System Requirements: {}", system_req);
 }
