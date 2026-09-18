470dd37398139ae287af4805c3c24652ac440e7c
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-18 19:59:00 +0530

    Practice mutable borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-fn main() {
-    let os_name = String::from("GaneshOS");
-    let kernel_os_name: String = os_name;
-    println!("{}", kernel_os_name);
-    let kernel_name = String::from("GaneshOS Kernel");
-    let kernel_name_clone = kernel_name.clone();
-    println!("{}\n{}", kernel_name, kernel_name_clone);
-    let sample_number: i8 = 16;
-    let sample_number_copy: i8 = sample_number;
-    println!("{}\n{}", sample_number, sample_number_copy);
+fn main(){
+    let mut os=String::from("GaneshOS");
+    add_version(&mut os);
+}
+fn add_version(os:&mut String){
+    os.push_str(" v0.1");
+    println!("{}",os);
 }
