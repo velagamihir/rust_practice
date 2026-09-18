7b6fea69641064d27ed068014d8aa6759af10bc9
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-18 21:34:54 +0530

    Practice multiple immutable borrows

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-fn main(){
-    let mut os=String::from("GaneshOS");
-    add_version(&mut os);
+fn main() {
+    let os_name: String = String::from("GaneshOS");
+    let reference1: &String = &os_name;
+    let reference2: &String = &os_name;
+    let reference3: &String = &os_name;
+    inspect(reference1, reference2, reference3);
 }
-fn add_version(os:&mut String){
-    os.push_str(" v0.1");
-    println!("{}",os);
+fn inspect(reference1: &String, reference2: &String, reference3: &String) {
+    println!("Inspector 1: {}", reference1);
+    println!("Inspector 2: {}", reference2);
+    println!("Inspector 3: {}", reference3);
 }
