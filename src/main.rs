9ec448294393d65473e81b942c743f0b769cbdf8
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-18 21:42:57 +0530

    Practice borrowing scope

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 fn main() {
-    let os_name: String = String::from("GaneshOS");
-    let reference1: &String = &os_name;
-    let reference2: &String = &os_name;
-    let reference3: &String = &os_name;
-    inspect(reference1, reference2, reference3);
-}
-fn inspect(reference1: &String, reference2: &String, reference3: &String) {
-    println!("Inspector 1: {}", reference1);
-    println!("Inspector 2: {}", reference2);
-    println!("Inspector 3: {}", reference3);
+    let mut os_name: String = String::from("GaneshOS");
+    let os: &String = &os_name;
+    println!("Checking OS...{}", os);
+    let os_name_reference: &mut String = &mut os_name;
+    os_name_reference.push_str(" Kernel");
+    println!("{}", os_name_reference);
 }
