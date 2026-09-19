3023e3da0cbee32d908a3b0d7cf6c6afed792068
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-19 18:14:30 +0530

    Practice Mutable Arrays

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let cpu_cores = [0, 1, 2, 3];
-    println!("CPU Cores: {:?}", cpu_cores);
-    println!("First Core: {}", cpu_cores[0]);
-    println!("Third Core: {}", cpu_cores[2]);
-    println!("Number of Cores: {}", cpu_cores.len());
+    let mut sample_array = [0, 1, 2, 3];
+    println!("Initial Array: {:?}", sample_array);
+    sample_array[2] = 8;
+    println!("Updated array: {:?}", sample_array);
+    println!("Third Core: {}", sample_array[2]);
 }
