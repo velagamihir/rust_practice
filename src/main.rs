cd8da6704893b161b7bf218861fe9c62bc815be8
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-19 18:53:24 +0530

    Learn iterating over array slices

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
     let ganeshos_cores = [0, 1, 2, 3, 4, 5];
     let group2_cores: &[i32] = &ganeshos_cores[2..5];
-    let no_of_cores: usize = inspect_cores(group2_cores);
-    println!("Number of cores: {}", no_of_cores);
+    //Variable to store the sum of all the elements in group2_cores slice
+    let sum: i32 = sum_cores(group2_cores);
+    println!("Sum of Core IDs: {}", sum);
 }
-fn inspect_cores(group2_cores: &[i32]) -> usize {
-    // Returning the number of cores in the group
-    group2_cores.len()
+// Function to calculate the sum of all the elements in an array slice
+fn sum_cores(cores: &[i32]) -> i32 {
+    let mut sum: i32 = 0;
+    for num in cores {
+        sum += num;
+    }
+    sum
 }
