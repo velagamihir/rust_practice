bba2e6f01ea294a82b48e0e7481286782342c801
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-19 17:56:39 +0530

    Using string slices in function as parameters

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+// Main function
 fn main() {
-    let full_name = String::from("GaneshOS Kernel");
-    let os_name: &str = &full_name[0..8];
-    let kernel_name: &str = &full_name[9..15];
-    println!(
-        "OS: {}\nComponent: {}\nFull Name: {}",
-        os_name, kernel_name, full_name
-    );
+    let full_os_name = String::from("GaneshOS Kernel");
+    let mut os_component_name: &str = &full_os_name;
+    check_component(os_component_name);
+    os_component_name = &full_os_name[9..15];
+    check_component(os_component_name);
+}
+// Function to check the os component
+fn check_component(os_component_name: &str) {
+    println!("Checking: {}...", os_component_name);
 }
