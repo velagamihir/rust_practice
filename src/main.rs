8977b036be653a87c7520e5a28bb1041c8cd9530
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-20 14:58:48 +0530

    Learn slice ranges and boundaries

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
-    let cpu_group1 = [0, 1, 2, 3];
-    let cpu_group2 = [4, 5, 6, 7, 8, 9];
-    let cpu_group3 = [10, 11];
-    println!("CPU Group1: {}", inspect_cpu_group(&cpu_group1));
-    println!("CPU Group2: {}", inspect_cpu_group(&cpu_group2));
-    println!("CPU Group3: {}", inspect_cpu_group(&cpu_group3));
+    let cpu_cores = [0, 1, 2, 3, 4, 5, 6, 7];
+    let cpu_cores_group1 = &cpu_cores[..2];
+    let cpu_cores_group2 = &cpu_cores[3..6];
+    let cpu_cores_group3 = &cpu_cores[6..];
+    println!(
+        "Group1: {}\nGroup2: {}\nGroup3: {}",
+        inspect_group(cpu_cores_group1),
+        inspect_group(cpu_cores_group2),
+        inspect_group(cpu_cores_group3)
+    )
 }
-fn inspect_cpu_group(cores_group: &[i32]) -> usize {
-    cores_group.len()
+fn inspect_group(cpu_cores_group: &[i32]) -> usize {
+    cpu_cores_group.len()
 }
