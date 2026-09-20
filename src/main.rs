1c11f8a597de2e7c3d085dfb8037ec982026b3da
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-20 14:39:16 +0530

    Learn arrays and slices in function parameters

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
 fn main() {
-    let mut cpu_cores: [i32; 6] = [1, 0, 0, 1, 0, 1];
-    println!("Cores before updation: {:?}", cpu_cores);
-    let cpu_cores_group: &mut [i32] = &mut cpu_cores[1..5];
-    let activated_cores: i8 = activate_cores(cpu_cores_group);
-    println!(
-        "Inactive cores that were now activated: {}",
-        activated_cores
-    );
-    println!("Updated cores: {:?}", cpu_cores);
+    let cpu_group1 = [0, 1, 2, 3];
+    let cpu_group2 = [4, 5, 6, 7, 8, 9];
+    let cpu_group3 = [10, 11];
+    println!("CPU Group1: {}", inspect_cpu_group(&cpu_group1));
+    println!("CPU Group2: {}", inspect_cpu_group(&cpu_group2));
+    println!("CPU Group3: {}", inspect_cpu_group(&cpu_group3));
 }
-// Function to activate only the inactive cores
-fn activate_cores(cpu_cores_group: &mut [i32]) -> i8 {
-    let mut activated_cores: i8 = 0;
-    for core in cpu_cores_group {
-        if *core == 0 {
-            *core = 1;
-            activated_cores += 1;
-        }
-    }
-    activated_cores
+fn inspect_cpu_group(cores_group: &[i32]) -> usize {
+    cores_group.len()
 }
