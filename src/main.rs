56404c35583c5fbc325469fbf8c1583d118fa880
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-20 14:08:03 +0530

    Learn Mutable Array slices

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
-    let ganeshos_cores = [0, 1, 2, 3, 4, 5];
-    let group2_cores: &[i32] = &ganeshos_cores[2..5];
-    //Variable to store the sum of all the elements in group2_cores slice
-    let sum: i32 = sum_cores(group2_cores);
-    println!("Sum of Core IDs: {}", sum);
+    let mut cpu_cores = [0, 0, 0, 0, 0, 0];
+    let cpu_cores_slice: &mut [i32] = &mut cpu_cores[2..5];
+    let activated_cores: usize = activate_cores(cpu_cores_slice);
+    println!("Activated cores: {}", activated_cores);
+    println!("Updated Cores: {:?}", cpu_cores);
 }
-// Function to calculate the sum of all the elements in an array slice
-fn sum_cores(cores: &[i32]) -> i32 {
-    let mut sum: i32 = 0;
-    for num in cores {
-        sum += num;
+fn activate_cores(cpu_cores_slice: &mut [i32]) -> usize {
+    let mut cores_activated = 0;
+    for core in cpu_cores_slice {
+        *core = 1;
+        cores_activated += 1;
     }
-    sum
+    cores_activated
 }
