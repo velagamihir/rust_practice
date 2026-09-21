802bba941f2bf6ff2012ae6f33a0191165e5e151
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-21 22:43:55 +0530

    Complete arrays and slices CPU Manager

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-fn main() {
-    let cpu_cores = [0, 1, 2, 3, 4, 5, 6, 7];
-    let cpu_cores_group1 = &cpu_cores[..2];
-    let cpu_cores_group2 = &cpu_cores[3..6];
-    let cpu_cores_group3 = &cpu_cores[6..];
-    println!(
-        "Group1: {}\nGroup2: {}\nGroup3: {}",
-        inspect_group(cpu_cores_group1),
-        inspect_group(cpu_cores_group2),
-        inspect_group(cpu_cores_group3)
-    )
+fn count_active_cores(cores: &[i32]) -> usize {
+    let mut active_cores = 0;
+    for core in cores {
+        if *core == 1 {
+            active_cores += 1;
+        }
+    }
+    active_cores
 }
-fn inspect_group(cpu_cores_group: &[i32]) -> usize {
-    cpu_cores_group.len()
+
+fn active_inactive_cores(cores: &mut [i32]) -> usize {
+    let mut activated_cores: usize = 0;
+    for core in cores {
+        if *core == 0 {
+            *core = 1;
+            activated_cores += 1;
+        }
+    }
+    activated_cores
+}
+
+fn total_cores(cores: &[i32]) -> usize {
+    cores.len()
+}
+
+fn main() {
+    let mut cpu_cores = [1, 0, 1, 0, 0, 1];
+    let group1_cores: &mut [i32] = &mut cpu_cores[..3];
+    let group2_cores: &mut [i32] = &mut cpu_cores[3..];
+    let total_cores_group1: usize = total_cores(group1_cores);
+    let active_cores_group1: usize = count_active_cores(group1_cores);
+    println!(
+        "Total Cores: {}\nActive Cores: {}",
+        total_cores_group1, active_cores_group1
+    );
+    let activated_cores_group1 = active_inactive_cores(group1_cores);
+    println!("Actived count: {}", activated_cores_group1);
+    // Activating group 2
+    let activated_cores_group2: usize = active_inactive_cores(group2_cores);
+    println!("Activated cores group2: {}", activated_cores_group2);
+    println!("Updated Cores: {:?}", cpu_cores);
 }
