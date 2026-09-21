59cd57b45d5d359c647c5ebd80bb61f6ba7aecb8
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-21 22:55:46 +0530

    Learn Basic Rust Structs

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,18 @@
-fn count_active_cores(cores: &[i32]) -> usize {
-    let mut active_cores = 0;
-    for core in cores {
-        if *core == 1 {
-            active_cores += 1;
-        }
-    }
-    active_cores
-}
-
-fn active_inactive_cores(cores: &mut [i32]) -> usize {
-    let mut activated_cores: usize = 0;
-    for core in cores {
-        if *core == 0 {
-            *core = 1;
-            activated_cores += 1;
-        }
-    }
-    activated_cores
-}
-
-fn total_cores(cores: &[i32]) -> usize {
-    cores.len()
-}
-
 fn main() {
-    let mut cpu_cores = [1, 0, 1, 0, 0, 1];
-    let group1_cores: &mut [i32] = &mut cpu_cores[..3];
-    let group2_cores: &mut [i32] = &mut cpu_cores[3..];
-    let total_cores_group1: usize = total_cores(group1_cores);
-    let active_cores_group1: usize = count_active_cores(group1_cores);
+    struct CpuCore {
+        id: i32,
+        state: i32,
+        frequency: i32,
+    }
+    let mut cpu_core: CpuCore = CpuCore {
+        id: 0,
+        state: 0,
+        frequency: 3200,
+    };
     println!(
-        "Total Cores: {}\nActive Cores: {}",
-        total_cores_group1, active_cores_group1
+        "Cpu Core Id: {}\nCPU Core State: {}\nCPU Core frequency: {}",
+        cpu_core.id, cpu_core.state, cpu_core.frequency
     );
-    let activated_cores_group1 = active_inactive_cores(group1_cores);
-    println!("Actived count: {}", activated_cores_group1);
-    // Activating group 2
-    let activated_cores_group2: usize = active_inactive_cores(group2_cores);
-    println!("Activated cores group2: {}", activated_cores_group2);
-    println!("Updated Cores: {:?}", cpu_cores);
+    cpu_core.state = 1;
+    println!("Updated State: {}", cpu_core.state);
 }
