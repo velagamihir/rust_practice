76b43e57c47cae6aad4c934edcc96d072e6ef7b3
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-22 20:38:49 +0530

    learn: use Option is_some to check for a value and unwrap to extract it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-fn get_name() -> Option<String> {
-    Some(String::from("Mihir"))
+fn get_name() -> Option<i32> {
+    Some(50)
 }
 fn main() {
-    let result = get_name();
-    if let Some(x) = result {
-        println!("Name: {}", x);
+    let result: Option<i32> = get_name();
+    if result.is_some() {
+        println!("Value Exists\nValue: {}", result.unwrap());
     }
 }
