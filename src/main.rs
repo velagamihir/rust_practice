e3789b6e4b805125d32922ea4078c5791c8cabbf
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-22 18:54:16 +0530

    feat: add enum-based OS event handling with pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+enum Event {
+    KeyPress { ch: char },
+    MouseClick { x: i32, y: i32 },
+    Resize { width: u32, height: u32 },
+    Quit,
+}
 fn main() {
-    struct CpuCore {
-        id: i32,
-        state: i32,
-        frequency: i32,
-    }
-    let mut cpu_core: CpuCore = CpuCore {
-        id: 0,
-        state: 0,
-        frequency: 3200,
+    let event1 = Event::KeyPress { ch: 'A' };
+    let event2 = Event::MouseClick { x: 90, y: 80 };
+    let event3 = Event::Resize {
+        width: 1800,
+        height: 1600,
     };
-    println!(
-        "Cpu Core Id: {}\nCPU Core State: {}\nCPU Core frequency: {}",
-        cpu_core.id, cpu_core.state, cpu_core.frequency
-    );
-    cpu_core.state = 1;
-    println!("Updated State: {}", cpu_core.state);
+    let event4 = Event::Quit;
+    handle_event(event1);
+    handle_event(event2);
+    handle_event(event3);
+    handle_event(event4);
+}
+fn handle_event(event: Event) {
+    match event {
+        Event::KeyPress { ch } => println!("Key Pressed: {}", ch),
+        Event::MouseClick { x, y } => println!("Mouse clicked at ({},{})", x, y),
+        Event::Resize { width, height } => println!("Window Resized to {}*{}", width, height),
+        Event::Quit => println!("System quitting"),
+    }
 }
