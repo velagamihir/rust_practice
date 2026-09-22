8eabf45ac58254ada02a10af82d3aa974480f6b4
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-22 19:14:26 +0530

    Learned Option<T> enum

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,10 @@
-enum Command {
-    Add(i32, i32),
-    Subtract(i32, i32),
-    Multiply(i32, i32),
-    Quit,
+fn get_number() -> Option<i32> {
+    Some(10)
 }
 fn main() {
-    let add = Command::Add(10, 5);
-    let subtract = Command::Subtract(10, 5);
-    let product = Command::Multiply(10, 5);
-    let quit = Command::Quit;
-    let add_result = execute_command(add);
-    let sub_result = execute_command(subtract);
-    let prod_result = execute_command(product);
-    let quit_result = execute_command(quit);
-    println!(
-        "{}\n{}\n{}\n{}",
-        add_result, sub_result, prod_result, quit_result
-    );
-}
-fn execute_command(command: Command) -> i32 {
-    let result = match command {
-        Command::Add(x, y) => x + y,
-        Command::Subtract(x, y) => x - y,
-        Command::Multiply(x, y) => x * y,
-        Command::Quit => 0,
-    };
-    result
+    let result = get_number();
+    match result {
+        Some(x) => println!("Result is: {}", x),
+        None => println!("None"),
+    }
 }
