b206c288168ffc5620ffe02ef51985ff38bf6b26
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-24 22:31:44 +0530

    propagate errors through multiple fallible operations with question  mark

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 fn get_number() -> Result<i32, String> {
     Ok(20)
 }
+fn add_ten(value: i32) -> Result<i32, String> {
+    Ok(value + 10)
+}
+fn divide(value: i32, divisor: i32) -> Result<i32, String> {
+    if divisor == 0 {
+        return Err(String::from("Cannot divide by zero"));
+    }
+    Ok(value / divisor)
+}
 fn calculate() -> Result<i32, String> {
     let result = get_number()?;
-    Ok(result + 10)
+    let result = add_ten(result)?;
+    let result = divide(result, 2)?;
+    Ok(result)
 }
 fn main() {
     let result = calculate();
