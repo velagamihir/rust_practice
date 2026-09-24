79b4ced56b92b087a953f5e9e7dfffbc20bd2c61
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-24 22:42:45 +0530

    learned map_err to format the error messages

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-fn get_number() -> Result<i32, String> {
-    Ok(20)
-}
-fn add_ten(value: i32) -> Result<i32, String> {
-    Ok(value + 10)
-}
-fn divide(value: i32, divisor: i32) -> Result<i32, String> {
-    if divisor == 0 {
-        return Err(String::from("Cannot divide by zero"));
-    }
-    Ok(value / divisor)
-}
-fn calculate() -> Result<i32, String> {
-    let result = get_number()?;
-    let result = add_ten(result)?;
-    let result = divide(result, 2)?;
-    Ok(result)
+fn get_score() -> Result<i32, String> {
+    Err(String::from("Score unavailable"))
 }
 fn main() {
-    let result = calculate();
-    println!("Result: {:?}", result);
+    let result = get_score();
+    println!(
+        "{:?}",
+        result.map_err(|x| format!("Failed to get Score: {}", x))
+    );
 }
