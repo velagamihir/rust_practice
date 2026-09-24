791640549027a865be819b35461e7985314dc131
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-24 22:23:31 +0530

    use question mark operator to propagate Result errors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-fn divide(a: i32, b: i32) -> Result<i32, String> {
-    if b == 0 {
-        return Err(String::from("Not able to divide by zero"));
-    }
-    Ok(a / b)
+fn get_number() -> Result<i32, String> {
+    Ok(20)
+}
+fn calculate() -> Result<i32, String> {
+    let result = get_number()?;
+    Ok(result + 10)
 }
 fn main() {
-    let b = 2;
-    let result: Result<i32, String> = Ok(20).and_then(|x| divide(x, b));
+    let result = calculate();
     println!("Result: {:?}", result);
 }
