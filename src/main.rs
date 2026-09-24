5504e44598761709a1c5326e70b7e97f688d1ba4
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-24 19:50:42 +0530

    use Result::unwrap_or to provide fallback value for errors

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-fn check_number(number: i32) -> Result<i32, String> {
-    if number >= 0 {
-        return Ok(number);
-    }
-    return Err(String::from("Number is negative"));
+fn get_score() -> Result<i32, String> {
+    Err(String::from("Score unavailable"))
 }
 fn main() {
-    let result: Result<i32, String> = check_number(10);
-    if result.is_ok() {
-        println!("Number is positive: {}", result.ok().unwrap());
-    } else {
-        println!("Number is not positive");
-    }
+    let result: Result<i32, String> = get_score();
+    println!("Result: {}", result.unwrap_or(0));
 }
