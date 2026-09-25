7b0ca41261b86e3ec4db5340b41772980b9968ed
Mihir Velaga <velaga.mihir@gmail.com>
2026-09-25 19:18:55 +0530

    combine Result validation and error propagation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 fn get_score() -> Result<i32, String> {
-    Err(String::from("Score unavailable"))
+    Ok(201)
+}
+fn add_bonus(score: i32) -> Result<i32, String> {
+    Ok(score + 10)
+}
+fn validate_score(score: i32) -> Result<i32, String> {
+    if score > 100 {
+        return Err(String::from("Score greater than 100")).map_err(|x| format!("Error: {}", x));
+    }
+    Ok(score)
+}
+fn calculate() -> Result<i32, String> {
+    let score = get_score()?;
+    let score = add_bonus(score)?;
+    let score = validate_score(score);
+    score
 }
 fn main() {
-    let score: i32 = get_score().unwrap_or_default();
-    println!("Score: {}", score);
+    let score = calculate();
+    println!("Result: {:?}", score);
 }
